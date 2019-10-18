e1f793da62bea471e61cb3080d56f51e8d0c5541
Светлана Сорокина <cveta2500@outlook.com>
2019-10-18 16:36:11 +0700

    Добавлен скрипт, gitignore и readme

---
+++ myscript.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+echo Input: the quick brown fox jumps over the lazy dog
+echo Expected Output: 
+echo brown 1
+echo dog 1
+echo fox 1
+echo jumps 1
+echo lazy 1
+echo over 1
+echo quick1
+echo the 2
+echo Actual utput:
+java -jar app.jar the quick brown fox jumps over the lazy dog
