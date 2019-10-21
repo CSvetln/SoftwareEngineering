dfd578146493b9fed24d45318c3e64f9b91b2f02
Светлана Сорокина <cveta2500@outlook.com>
2019-10-21 19:52:17 +0700

    16 пункт плана

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,8 @@
-fun helpOut() {
-    println("1")
-    println(
-            """Приложение аутентифицирует пользователя по логину и паролю
-        Коды возврата:
-        0 - успех
-        1 - справка  
-        2 - неверный формат логина
-        3 - неизвестный логин 
-        4 - неправильный пароль """
-    )
-}
 fun main(args: Array<String>) {
-
   /*  for(p in args){
         println(p)
     }
     print(args.size)*/
-
     if(args.isNullOrEmpty())
         helpOut()
     if(args[0]=="-h")
@@ -32,7 +18,52 @@ fun main(args: Array<String>) {
     }
 }
 
+class Params(args: Array<String>)
+{
+    var login:String =""
+    var pass:String=""
+    var isHelp: Boolean =false
 
+    init{
+        if(args.size==4) {
+            login = args[1]
+            pass = args[3]
+        }
+        if(args[0]=="-h")
+            isHelp=true
 
+    }
+}
+class ValidateServise(val logins:List<String>)
+{
+    val pat =Regex("""[0-9a-z]+""")
+    fun isLoginValid(login: String):Boolean
+    {
+       return login.matches(pat)
+    }
+    fun findUser(login: String):Boolean
+    {
+        for(log in logins) {
+            return log == login
+        }
+        return false
+    }
+   /* fun isPassCorrect(login: String, pass: String):Boolean
+    {
+
+    }*/
+}
 
 data class User(val login:String, val pass:String)
+fun helpOut() {
+    println("1")
+    println(
+            """Приложение аутентифицирует пользователя по логину и паролю
+        Коды возврата:
+        0 - успех
+        1 - справка  
+        2 - неверный формат логина
+        3 - неизвестный логин 
+        4 - неправильный пароль """
+    )
+}
