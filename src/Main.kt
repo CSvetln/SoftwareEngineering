80f3097c47a767c53d1fac4686bf1e8981108fcb
Светлана Сорокина <cveta2500@outlook.com>
2019-10-23 18:34:51 +0700

    Отформатировала код

--- src/Main.kt
+++ src/Main.kt
@@ -8,12 +8,12 @@ fun main(args: Array<String>) {
         val par = Params(args)
         val validService = ValidateService()
 
-        if(par.isHelp)
+        if (par.isHelp)
             helpOut()
         else {
-            if(!validService.isLoginValid(par.login))
+            if (!validService.isLoginValid(par.login))
                 println(2)
-            else{
+            else {
                 val us: User = validService.findUser(par.login)!!
 
                 if (validService.isPassCorrect(us, par.hash))
@@ -22,9 +22,7 @@ fun main(args: Array<String>) {
                     println(4)
             }
         }
-    }
-    catch (e: KotlinNullPointerException)
-    {
+    } catch (e: KotlinNullPointerException) {
         println(3)
     }
 }
@@ -36,21 +34,19 @@ class Params(args: Array<String>) {
     var isHelp: Boolean = false
 
     init {
-        if(args.isNotEmpty()) {
+        if (args.isNotEmpty()) {
             if (args[0] == "-h")
                 isHelp = true
             else {
                 if ((args[0] == "-login") and (args[2] == "-pass")) {
                     login = args[1]
                     hash = getHash(args[3])
-                }
-                else if ((args[2] == "-login") and (args[0] == "-pass")) {
+                } else if ((args[2] == "-login") and (args[0] == "-pass")) {
                     login = args[3]
                     hash = getHash(args[1])
                 }
             }
-        }
-        else
+        } else
             isHelp = true
     }
 }
@@ -59,44 +55,43 @@ val users = listOf(User("admin", getHash("admin")), User("user1", getHash("user"
 
 
 class ValidateService {
-    private val pat =Regex("""[0-9a-z]+""")
+    private val pat = Regex("""[0-9a-z]+""")
 
-    fun isLoginValid(login: String):Boolean
-    {
-       return login.matches(pat)
+    fun isLoginValid(login: String): Boolean {
+        return login.matches(pat)
     }
 
-    fun findUser(login: String):User?
-    {
-        for(user in users) {
-           if(user.login==login)
-               return user
-            }
+    fun findUser(login: String): User? {
+        for (user in users) {
+            if (user.login == login)
+                return user
+        }
         return null
     }
 
-    fun isPassCorrect(user:User, hash: String):Boolean
-    {
-      return hash.equals(user.hash)
+    fun isPassCorrect(user: User, hash: String): Boolean {
+        return hash.equals(user.hash)
     }
 
 }
-fun getHash(pass:String):String{
+
+fun getHash(pass: String): String {
 
     val md = MessageDigest.getInstance("MD5")
     md.update(pass.toByteArray());
-    val byteData:ByteArray = md.digest();
+    val byteData: ByteArray = md.digest();
     val sb = StringBuffer()
     for (aByteData in byteData) {
         sb.append(Integer.toString((aByteData and 0xff.toByte()) + 0x100, 16).substring(1))
     }
     return sb.toString()
 }
-data class User(val login:String, val hash:String)
+
+data class User(val login: String, val hash: String)
 
 fun helpOut() {
     println("1")
- println(
+    println(
             """Приложение аутентифицирует пользователя по логину и паролю
         Коды возврата:
         0 - успех
