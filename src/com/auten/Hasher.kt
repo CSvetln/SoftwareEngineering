718cf4f43110ba46a09095f0a98091eeae5efb7d
Светлана Сорокина <cveta2500@outlook.com>
2019-10-26 22:43:04 +0700

    Поменяла метод хеширования на тот который нашли на занятии

---
+++ src/com/auten/Hasher.kt
@@ -0,0 +1,14 @@
+package com.auten.softwareengineering
+
+import java.security.MessageDigest
+
+class Hasher { //класс для хеширрования
+    companion object {
+        fun getHash(pass: String): String {
+            val bytes = pass.toByteArray()
+            val md = MessageDigest.getInstance("SHA-256")
+            val digest = md.digest(bytes)
+            return digest.fold("", { str, it -> str + "%02x".format(it) })
+        }
+    }
+}
