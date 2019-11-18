5fe4eb26dbd8cf01ddd43d82b9c01f1ad565ad1e
Светлана Сорокина <cveta2500@outlook.com>
2019-11-18 19:32:54 +0700

    Добавила и распарсила поля даты

--- src/com/auten/Params.kt
+++ src/com/auten/Params.kt
@@ -3,6 +3,9 @@ package com.auten.softwareengineering
 import com.main.softwareengineering.accesses
 import com.main.softwareengineering.users
 import kotlinx.cli.*
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class Params(private val args: Array<String>) {
     private val isHelp: Boolean
@@ -11,12 +14,25 @@ class Params(private val args: Array<String>) {
     private val pass by parser.option(ArgType.String, shortName = "pass").default(" ")
     private val res by parser.option(ArgType.String, shortName = "res").default(" ")
     private val role by parser.option(ArgType.String, shortName = "role").default(" ")
+    private val dss by parser.option(ArgType.String, shortName = "ds").default(" ")
+    private val dee by parser.option(ArgType.String, shortName = "de").default(" ")
+    private val vol by parser.option(ArgType.Int, shortName= "vol").default(0)
+    private var ds:LocalDate?
+    private var de:LocalDate?
 
     private val validService = ValidateService(users, accesses)
 
     init {
         parser.parse(args)
         isHelp = args.isNullOrEmpty() or (args.size == 1)
+        if (validService.isDateValid(dss))
+            ds = LocalDate.parse(dss)
+        else
+            ds = null
+        if (validService.isDateValid(dee))
+            de = LocalDate.parse(dee)
+        else
+            de = null
     }
 
 
@@ -54,5 +70,6 @@ class Params(private val args: Array<String>) {
             }
         }
     }
+
 }
 
