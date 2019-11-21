e462981f0e5be7279738933a99abf0797f5689c3
Светлана Сорокина <cveta2500@outlook.com>
2019-11-21 19:34:09 +0700

    Измененила новый набор тестовых данных

--- src/com/Main.kt
+++ src/com/Main.kt
@@ -27,10 +27,10 @@ val accesses = listOf(
         Access("admin", Roles.WRITE, "AB"),
         Access("admin", Roles.READ, "AB.C"),
         Access("user1", Roles.EXECUTE, "AB.CD.E"),
-        Access("John Doe", Roles.READ, "a"),
-        Access("John Doe", Roles.WRITE, "a.b"),
-        Access("Jane Row", Roles.EXECUTE, "a.b.c"),
-        Access("John Doe", Roles.EXECUTE, "a.bc")
+        Access("jdoe", Roles.READ, "a"),
+        Access("jdoe", Roles.WRITE, "a.b"),
+        Access("jrow", Roles.EXECUTE, "a.b.c"),
+        Access("jdoe", Roles.EXECUTE, "a.bc")
 )
 
 
