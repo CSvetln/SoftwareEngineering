3d1a5cb3831a05bd32a2ba724ea3855bc27c8679
Светлана Сорокина <cveta2500@outlook.com>
2019-11-21 20:57:44 +0700

    kotlinx в lib

--- Sc1.sh
+++ Sc1.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-kotlinc -cp kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar src/com -include-runtime -d app.jar
+kotlinc -cp lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar src/com -include-runtime -d app.jar
 amountDefTests=0
 amountSucTests=0
 
