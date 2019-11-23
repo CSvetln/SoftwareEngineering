74e070fe25d84dde7846ac76e4b22eb4cd6bb0c0
Светлана Сорокина <cveta2500@outlook.com>
2019-11-24 00:27:43 +0700

    Изменение в скрипте

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-java -cp "lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar;app.jar" com.softwareengineering.MainKt "$@"
+java -cp "lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar:app.jar" com.softwareengineering.MainKt "$@"
