2bfc723237fef483f50033348397955282ffefa7
Светлана Сорокина <cveta2500@outlook.com>
2020-01-02 17:40:56 +0700

    Подключила flyway.jar

--- TEST.sh
+++ TEST.sh
@@ -4,11 +4,11 @@ amountSucTests=0
 res=0
 platform='unknown'
 if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-platform=';'
+  platform=';'
 else
   platform=':'
 fi
-libs="lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar${platform}lib/log4j-api-2.12.1.jar${platform}lib/log4j-core-2.12.1.jar${platform}lib/h2-1.4.200.jar"
+libs="lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar${platform}lib/log4j-api-2.12.1.jar${platform}lib/log4j-core-2.12.1.jar${platform}lib/h2-1.4.200.jar${platform}lib/flyway-core-6.1.1.jar"
 kotlinc -verbose -cp "$libs" src/com -include-runtime -d app.jar
 jar -u -f app.jar -m src/META-INF/MANIFEST.MF -C resources .
 
@@ -30,19 +30,19 @@ fh 2 "-login X-X -pass XXX"
 fh 3 "-login XXX -pass XXX"
 fh 4 "-login jdoe -pass XXX"
 fh 0 "-login jdoe -pass sup3rpaZZ"
-fh 0 "-login jdoe -pass sup3rpaZZ -role READ -res a"
-fh 0 "-login jdoe -pass sup3rpaZZ -role READ -res a.b"
-fh 5 "-login jdoe -pass sup3rpaZZ -role XXX -res a.b"
+fh 0 "-login jdoe -pass sup3rpaZZ -role READ -res A"
+fh 0 "-login jdoe -pass sup3rpaZZ -role READ -res A.B"
+fh 5 "-login jdoe -pass sup3rpaZZ -role XXX -res A.B"
 fh 6 "-login jdoe -pass sup3rpaZZ -role READ -res XXX"
-fh 6 "-login jdoe -pass sup3rpaZZ -role WRITE -res a"
-fh 6 "-login jdoe -pass sup3rpaZZ -role WRITE -res a.bc"
-fh 0 "-login jdoe -pass sup3rpaZZ -role READ -res a.b -ds 2015-01-01 -de 2015-12-31 -vol 100"
-fh 7 "-login jdoe -pass sup3rpaZZ -role READ -res a.b -ds 01-01-2015 -de 2015-12-31 -vol 100"
-fh 7 "-login jdoe -pass sup3rpaZZ -role READ -res a.b -ds 2015-01-01 -de 2015-12-31 -vol XXX"
+fh 6 "-login jdoe -pass sup3rpaZZ -role WRITE -res A"
+fh 6 "-login jdoe -pass sup3rpaZZ -role WRITE -res A.BC"
+fh 0 "-login jdoe -pass sup3rpaZZ -role READ -res A.B -ds 2015-01-01 -de 2015-12-31 -vol 100"
+fh 7 "-login jdoe -pass sup3rpaZZ -role READ -res A.B -ds 01-01-2015 -de 2015-12-31 -vol 100"
+fh 7 "-login jdoe -pass sup3rpaZZ -role READ -res A.B -ds 2015-01-01 -de 2015-12-31 -vol XXX"
 fh 3 "-login X -pass X -role READ -res X -ds 2015-01-01 -de 2015-12-31 -vol XXX"
 fh 3 "-login X -pass X -role READ -res X"
-fh 0 "-login admin -pass admin -role READ -res a.b.c"
-
+fh 0 "-login admin -pass admin -role READ -res AB.C.D"
+fh 0 "-login admin -pass admin -role READ -res A.C.D"
 echo Количество тестов
 echo $amountDefTests
 echo Количество успешных тестов
